dda2f39e75e01324c37c5773eed21334c70e0ae5
Tanner Townsend <rsmcwow@gmail.com>
2020-03-30 00:58:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     return "The line is currently empty."
   else 
-    katz_deli.each_with_index { |name,
+    message = "The line is currently: "
+    katz_deli.each_with_index do |name,index|
+      message += "#{index}. #{name} "
+    end
+    return message
 end
